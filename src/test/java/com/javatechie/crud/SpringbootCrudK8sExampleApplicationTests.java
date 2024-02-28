9916250de62e86b821472266d74e902edd74261d
abhipatil <abhipatil@cybage.com>
2024-02-28 17:48:43 +0530

    .

---
+++ src/test/java/com/javatechie/crud/SpringbootCrudK8sExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javatechie.crud;
+
+import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.singam.crud.SpringbootCrudK8sExampleApplication;
+
+@SpringBootTest(classes = SpringbootCrudK8sExampleApplication.class)
+class SpringbootCrudK8sExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
